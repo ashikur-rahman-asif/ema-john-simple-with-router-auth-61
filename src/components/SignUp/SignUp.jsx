ff75d37015d89e3807222cb6020e53b3707014f9
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-29 03:58:32 +0600

    auth context done

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
+const SignUp = () => {
+  const [error, setError]=useState('')
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    console.log(email, password, confirm);
+    if (password !== confirm) {
+      setError('your password did not match')
+      return
+    }
+    else if (password.length < 6) {
+      setError('password must be 6 characters longer')
+      return
+    }
+  };
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p>
+        <small>
+          Already have an account? <Link to="/login">Login</Link>
+        </small>
+      </p>
+      <p className='text-error'>{error}</p>
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOCQcQSKGg70QfKJg1F3EwGply7VQjYp8",
+  authDomain: "ema-john-with-firebase-a-65154.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-65154",
+  storageBucket: "ema-john-with-firebase-a-65154.appspot.com",
+  messagingSenderId: "817161641271",
+  appId: "1:817161641271:web:cede7947686c607cf44eb2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
